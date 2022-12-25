551be2badab0eeeb199fffdd86f5affb96dab36a
Emir <emirsoyalan79@gmail.com>
2022-12-25 03:47:55 +0300

    Revert default time format
    - Causes crash

--- schemas/message.py
+++ schemas/message.py
@@ -6,7 +6,7 @@ class Message(BaseModel):
     fromID:int = None
     toID:int = None
     content:str = None
-    date:datetime = Field(default_factory=datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+    date:datetime = Field(default_factory=datetime.now)
 
 class MessageSend(BaseModel):
     fromID:int
